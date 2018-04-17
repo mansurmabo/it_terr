c0a90ad1c18d5c64aaaa24ea14db8ceb50568c94
Mansur Mabo <mansurmabo@gmail.com>
2018-04-18 03:31:45 +0600

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ APP_ADDRESS = "http://localhost:3000"
 puts 'starting add data'
 
 # may changed it to more
-5.times do
+50.times do
   User.create(login: FFaker::Internet.email)
 end
 
 puts 'Users added'
 
 # may changed it to more
-20.times do
+100.times do
   User.all.each do |user|
     user.posts.create(
       title: FFaker::BaconIpsum.word,
@@ -23,19 +23,21 @@ end
 puts 'Posts added'
 
 # may changed it to more
-100.times do
-  Post.all.each do |post|
+50.times do
+  Post.take(50).each do |post|
     post.ratings.create(mark: Random.new.rand(1..5))
   end
 end
 puts 'Ratings added'
+
+# if you want to add data by controllers, make sure that you server is runnning or just commented code below
 
 20.times do
   HTTParty.post("#{APP_ADDRESS}/posts?login=#{FFaker::Internet.email}&title=#{FFaker::BaconIpsum.word}&description=#{FFaker::BaconIpsum.sentence}")
 end
 puts 'created users and posts by controller'
 
-Post.all.each do |post|
+Post.take(20).each do |post|
   HTTParty.post("#{APP_ADDRESS}/posts/#{post.id}/estimate?mark=#{Random.new.rand(1..5)}")
 end
 puts 'ratings added by controller'
